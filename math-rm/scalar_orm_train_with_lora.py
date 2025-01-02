e6acb9bdbaffd5366da3874a31ca5e80f1ba0b66
thanhnienyeumeo <nguyentronghoangmc@gmail.com>
2025-01-02 11:51:15 +0700

    add lora

---
+++ math-rm/scalar_orm_train_with_lora.py
@@ -0,0 +1,565 @@
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional, Union
+
+import numpy as np
+import torch
+import torch.nn as nn
+from datasets import load_dataset
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    HfArgumentParser,
+    Trainer,
+    TrainingArguments,
+)
+from transformers.utils import PaddingStrategy
+
+
+
+
+# Define and parse arguments.
+@dataclass
+class ScriptArguments:
+    """
+    These arguments vary depending on how many GPUs you have, what their capacity and features are, and what size model you want to train.
+    """
+    local_rank: Optional[int] = field(
+        default=-1, metadata={"help": "Used for multi-gpu"})
+
+    deepspeed: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "Path to deepspeed config if using deepspeed. You may need this if the model that you want to train doesn't fit on a single GPU."
+        },
+    )
+    per_device_train_batch_size: Optional[int] = field(default=1)
+    per_device_eval_batch_size: Optional[int] = field(default=1)
+    # for 8 GPU, the global batch size is 512
+    gradient_accumulation_steps: Optional[int] = field(default=2)
+    learning_rate: Optional[float] = field(default=2e-5)
+    weight_decay: Optional[float] = field(default=0.001)
+    model_name: Optional[str] = field(
+        default="Qwen/Qwen2.5-1.5B-Instruct",
+        metadata={
+            "help": "The model that you want to train from the Hugging Face hub. E.g. gpt2, gpt2-xl, bert, etc."
+        },
+    )
+    bf16: Optional[bool] = field(
+        default=True,
+        metadata={
+            "help": "This essentially cuts the training time in half if you want to sacrifice a little precision and have a supported GPU."
+        },
+    )
+    num_train_epochs: Optional[int] = field(
+        default=1,
+        metadata={"help": "The number of training epochs for the reward model."},
+    )
+    train_set_path: Optional[str] = field(
+        default="RLHFlow/Mistral-ORM-Data",
+        metadata={"help": "The dir of the subset of the training data to use"},
+    )
+    output_path: Optional[str] = field(
+        default="./model/qwen1.5b_orm",
+        metadata={"help": "The dir for output model"},
+    )
+    gradient_checkpointing: Optional[bool] = field(
+        default=True,
+        metadata={"help": "Enables gradient checkpointing."},
+    )
+    optim: Optional[str] = field(
+        # default="adamw_hf",
+        default="paged_adamw_32bit",
+        # default="adamw_torch_fused",
+        metadata={"help": "The optimizer to use."},
+    )
+    lr_scheduler_type: Optional[str] = field(
+        default="cosine",
+        metadata={"help": "The lr scheduler"},
+    )
+    max_length: Optional[int] = field(default=4096)
+
+    save_every_steps: Optional[int] = field(
+        default=2000,
+        metadata={"help": "Save the model every x steps"},
+    )
+    eval_every_steps: Optional[int] = field(
+        default=2000,
+        metadata={"help": "Eval the model every x steps"},
+    )
+
+
+parser = HfArgumentParser(ScriptArguments)
+script_args = parser.parse_args_into_dataclasses()[0]
+
+# Load the value-head model and tokenizer.
+tokenizer_name = script_args.model_name
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, use_fast = False)
+
+# Adjusted according to the base model
+# Need to do this for the models that don't have an official pad token.
+#tokenizer.pad_token = tokenizer.eos_token
+#tokenizer.pad_token_id = tokenizer.eos_token_id
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+print(tokenizer.padding_side)
+tokenizer.truncation_side = "left"
+tokenizer.model_max_length = script_args.max_length
+# tokenizer.padding_side = "right"
+
+
+
+# Get the dataset
+train_path = script_args.train_set_path
+# eval_path = script_args.eval_set_path
+output_name = script_args.output_path
+
+
+def build_dataset(tokenizer, train_path, eval_path = None):
+
+    def tokenize(sample):
+        question = sample['conversations'][0]['content'].split("Step 1")[0]
+        ans = "Step 1" + sample['conversations'][0]['content'].split("Step 1")[1]
+        message = [
+            {"role":"user", "content":question},
+            {"role":"assistant", "content":ans}
+        ]
+        sample['positive'] = tokenizer.apply_chat_template(
+            # message, tokenize=False, add_generation_prompt=False).replace(tokenizer.bos_token, "")
+            message, tokenize=False, add_generation_prompt=False)
+        tokenized_pos = tokenizer(sample['positive'], truncation=True)
+        sample["input_ids_j"] = tokenized_pos["input_ids"]
+        sample["attention_mask_j"] = tokenized_pos["attention_mask"]
+        if sample['conversations'][1]['content'] == '+':
+            sample['label'] = 1
+        elif sample['conversations'][1]['content'] == '-':
+            sample['label'] = 0
+        else:
+            assert 1 == 0
+        return sample
+
+    ds = load_dataset(train_path, split="train").shuffle(seed=42)
+    #ds = ds.select(range(2000))
+    ds = ds.map(tokenize, num_proc=8)
+
+    eval_dataset = None
+
+    train_dataset = ds
+    #eval_dataset = load_dataset(eval_path, split="train").shuffle(seed=42).select(range(500))
+    eval_dataset = ds.select(range(500))
+    return train_dataset, eval_dataset
+
+
+train_dataset, eval_dataset = build_dataset(tokenizer, train_path, eval_path = None)
+print("Training set: ", len(train_dataset), " Eval set: ", len(eval_dataset))
+
+# Define the trainer
+
+
+# Define the trainer
+training_args = TrainingArguments(
+    output_dir=output_name,
+    learning_rate=script_args.learning_rate,
+    per_device_train_batch_size=script_args.per_device_train_batch_size,
+    per_device_eval_batch_size=script_args.per_device_eval_batch_size,
+    num_train_epochs=script_args.num_train_epochs,
+    weight_decay=script_args.weight_decay,
+    evaluation_strategy="steps",
+    eval_steps=script_args.eval_every_steps,
+    save_strategy="steps",
+    save_steps=script_args.save_every_steps,
+    gradient_accumulation_steps=script_args.gradient_accumulation_steps,
+    gradient_checkpointing=script_args.gradient_checkpointing,
+    deepspeed=script_args.deepspeed,
+    local_rank=script_args.local_rank,
+    remove_unused_columns=False,
+    label_names=[],
+    bf16=script_args.bf16,
+    logging_strategy="steps",
+    logging_steps=100,
+    optim=script_args.optim,
+    lr_scheduler_type=script_args.lr_scheduler_type,
+    warmup_ratio=0.03,
+    report_to='wandb',
+    save_only_model = True
+)
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    script_args.model_name, num_labels=1, torch_dtype=torch.bfloat16, use_flash_attention_2=True,
+)
+
+model.config.use_cache = not script_args.gradient_checkpointing
+model.config.pad_token_id = tokenizer.pad_token_id
+model.resize_token_embeddings(len(tokenizer))
+
+num_proc = 24  # Can adjust to be higher if you have more processors.
+original_columns = train_dataset.column_names
+
+
+# We need to define a special data collator that batches the data in our j vs k format.
+@dataclass
+class RewardDataCollatorWithPadding:
+    tokenizer: AutoTokenizer
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+    return_tensors: str = "pt"
+
+    def __call__(self, features: List[Dict[str, Any]]) -> Dict[str, Any]:
+        merged_features = []
+        label_list = []
+        for feature in features:
+            merged_features.append(
+                {
+                    "input_ids": feature["input_ids_j"],
+                    "attention_mask": feature["attention_mask_j"],
+                }
+            )
+            label_list.append([feature['label']])
+    
+        batch = self.tokenizer.pad(
+            merged_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors=self.return_tensors,
+        )
+        batch = {
+            "input_ids": batch["input_ids"],
+            "attention_mask": batch["attention_mask"],
+            "label": label_list,
+            "return_loss": True,
+        }
+        return batch
+
+
+# Define the trainer
+# def compute_metrics(eval_pred):
+#     print(eval_pred)
+#     result = {}
+#     result['eval_loss'] = eval_pred.predictions[0]
+    
+#     return result
+
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional, Union
+
+import numpy as np
+import torch
+import torch.nn as nn
+from datasets import load_dataset
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    HfArgumentParser,
+    Trainer,
+    TrainingArguments,
+)
+from transformers.utils import PaddingStrategy
+
+
+
+
+# Define and parse arguments.
+@dataclass
+class ScriptArguments:
+    """
+    These arguments vary depending on how many GPUs you have, what their capacity and features are, and what size model you want to train.
+    """
+    local_rank: Optional[int] = field(
+        default=-1, metadata={"help": "Used for multi-gpu"})
+
+    deepspeed: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "Path to deepspeed config if using deepspeed. You may need this if the model that you want to train doesn't fit on a single GPU."
+        },
+    )
+    per_device_train_batch_size: Optional[int] = field(default=1)
+    per_device_eval_batch_size: Optional[int] = field(default=1)
+    # for 8 GPU, the global batch size is 512
+    gradient_accumulation_steps: Optional[int] = field(default=2)
+    learning_rate: Optional[float] = field(default=2e-5)
+    weight_decay: Optional[float] = field(default=0.001)
+    model_name: Optional[str] = field(
+        default="Qwen/Qwen2.5-1.5B-Instruct",
+        metadata={
+            "help": "The model that you want to train from the Hugging Face hub. E.g. gpt2, gpt2-xl, bert, etc."
+        },
+    )
+    bf16: Optional[bool] = field(
+        default=True,
+        metadata={
+            "help": "This essentially cuts the training time in half if you want to sacrifice a little precision and have a supported GPU."
+        },
+    )
+    num_train_epochs: Optional[int] = field(
+        default=1,
+        metadata={"help": "The number of training epochs for the reward model."},
+    )
+    train_set_path: Optional[str] = field(
+        default="RLHFlow/Mistral-ORM-Data",
+        metadata={"help": "The dir of the subset of the training data to use"},
+    )
+    output_path: Optional[str] = field(
+        default="./model/qwen1.5b_orm",
+        metadata={"help": "The dir for output model"},
+    )
+    gradient_checkpointing: Optional[bool] = field(
+        default=True,
+        metadata={"help": "Enables gradient checkpointing."},
+    )
+    optim: Optional[str] = field(
+        # default="adamw_hf",
+        default="paged_adamw_32bit",
+        # default="adamw_torch_fused",
+        metadata={"help": "The optimizer to use."},
+    )
+    lr_scheduler_type: Optional[str] = field(
+        default="cosine",
+        metadata={"help": "The lr scheduler"},
+    )
+    max_length: Optional[int] = field(default=4096)
+
+    save_every_steps: Optional[int] = field(
+        default=2000,
+        metadata={"help": "Save the model every x steps"},
+    )
+    eval_every_steps: Optional[int] = field(
+        default=2000,
+        metadata={"help": "Eval the model every x steps"},
+    )
+
+
+parser = HfArgumentParser(ScriptArguments)
+script_args = parser.parse_args_into_dataclasses()[0]
+
+# Load the value-head model and tokenizer.
+tokenizer_name = script_args.model_name
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, use_fast = False)
+
+# Adjusted according to the base model
+# Need to do this for the models that don't have an official pad token.
+#tokenizer.pad_token = tokenizer.eos_token
+#tokenizer.pad_token_id = tokenizer.eos_token_id
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+print(tokenizer.padding_side)
+tokenizer.truncation_side = "left"
+tokenizer.model_max_length = script_args.max_length
+# tokenizer.padding_side = "right"
+
+
+
+# Get the dataset
+train_path = script_args.train_set_path
+# eval_path = script_args.eval_set_path
+output_name = script_args.output_path
+
+
+def build_dataset(tokenizer, train_path, eval_path = None):
+
+    def tokenize(sample):
+        question = sample['conversations'][0]['content'].split("Step 1")[0]
+        ans = "Step 1" + sample['conversations'][0]['content'].split("Step 1")[1]
+        message = [
+            {"role":"user", "content":question},
+            {"role":"assistant", "content":ans}
+        ]
+        sample['positive'] = tokenizer.apply_chat_template(
+            # message, tokenize=False, add_generation_prompt=False).replace(tokenizer.bos_token, "")
+            message, tokenize=False, add_generation_prompt=False)
+        tokenized_pos = tokenizer(sample['positive'], truncation=True)
+        sample["input_ids_j"] = tokenized_pos["input_ids"]
+        sample["attention_mask_j"] = tokenized_pos["attention_mask"]
+        if sample['conversations'][1]['content'] == '+':
+            sample['label'] = 1
+        elif sample['conversations'][1]['content'] == '-':
+            sample['label'] = 0
+        else:
+            assert 1 == 0
+        return sample
+
+    ds = load_dataset(train_path, split="train").shuffle(seed=42)
+    #ds = ds.select(range(2000))
+    ds = ds.map(tokenize, num_proc=8)
+
+    eval_dataset = None
+
+    train_dataset = ds
+    #eval_dataset = load_dataset(eval_path, split="train").shuffle(seed=42).select(range(500))
+    eval_dataset = ds.select(range(500))
+    return train_dataset, eval_dataset
+
+
+train_dataset, eval_dataset = build_dataset(tokenizer, train_path, eval_path = None)
+print("Training set: ", len(train_dataset), " Eval set: ", len(eval_dataset))
+
+# Define the trainer
+
+
+# Define the trainer
+training_args = TrainingArguments(
+    output_dir=output_name,
+    learning_rate=script_args.learning_rate,
+    per_device_train_batch_size=script_args.per_device_train_batch_size,
+    per_device_eval_batch_size=script_args.per_device_eval_batch_size,
+    num_train_epochs=script_args.num_train_epochs,
+    weight_decay=script_args.weight_decay,
+    evaluation_strategy="steps",
+    eval_steps=script_args.eval_every_steps,
+    save_strategy="steps",
+    save_steps=script_args.save_every_steps,
+    gradient_accumulation_steps=script_args.gradient_accumulation_steps,
+    gradient_checkpointing=script_args.gradient_checkpointing,
+    deepspeed=script_args.deepspeed,
+    local_rank=script_args.local_rank,
+    remove_unused_columns=False,
+    label_names=[],
+    bf16=script_args.bf16,
+    logging_strategy="steps",
+    logging_steps=100,
+    optim=script_args.optim,
+    lr_scheduler_type=script_args.lr_scheduler_type,
+    warmup_ratio=0.03,
+    report_to='wandb',
+    save_only_model = True
+)
+
+
+
+num_proc = 24  # Can adjust to be higher if you have more processors.
+original_columns = train_dataset.column_names
+
+
+# We need to define a special data collator that batches the data in our j vs k format.
+@dataclass
+class RewardDataCollatorWithPadding:
+    tokenizer: AutoTokenizer
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+    return_tensors: str = "pt"
+
+    def __call__(self, features: List[Dict[str, Any]]) -> Dict[str, Any]:
+        merged_features = []
+        label_list = []
+        for feature in features:
+            merged_features.append(
+                {
+                    "input_ids": feature["input_ids_j"],
+                    "attention_mask": feature["attention_mask_j"],
+                }
+            )
+            label_list.append([feature['label']])
+    
+        batch = self.tokenizer.pad(
+            merged_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors=self.return_tensors,
+        )
+        batch = {
+            "input_ids": batch["input_ids"],
+            "attention_mask": batch["attention_mask"],
+            "label": label_list,
+            "return_loss": True,
+        }
+        return batch
+
+
+# Define the trainer
+# def compute_metrics(eval_pred):
+#     print(eval_pred)
+#     result = {}
+#     result['eval_loss'] = eval_pred.predictions[0]
+    
+#     return result
+
+
+from peft import LoraConfig, get_peft_model
+
+# Define LoRA parameters
+lora_config = LoraConfig(
+    r=8,  # LoRA rank
+    lora_alpha=16,  # Scaling factor
+    target_modules="all-linear",  # Modules to target
+    lora_dropout=0.1,  # Dropout rate
+    bias="none",  # Bias handling
+    task_type="SEQ_CLS"  # Sequence Classification
+)
+# Wrap the model with LoRA
+model = AutoModelForSequenceClassification.from_pretrained(
+    script_args.model_name, num_labels=1, torch_dtype=torch.bfloat16, use_flash_attention_2=True,
+)
+
+model.config.use_cache = not script_args.gradient_checkpointing
+model.config.pad_token_id = tokenizer.pad_token_id
+model.resize_token_embeddings(len(tokenizer))
+# model = get_peft_model(model, lora_config)
+
+class RewardTrainer(Trainer):
+     def __init__(self, *args, lora_config=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if lora_config:
+            self.model = get_peft_model(self.model, lora_config)
+            print("LoRA applied to the model!")
+     def compute_loss(self, model, inputs, num_items_in_batch = None, return_outputs=False):
+        outputs = model(
+            input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"]
+        )
+        label = inputs['label']
+        label = torch.tensor(label,dtype=torch.bfloat16).to(outputs.logits.device)
+        probs = torch.sigmoid(outputs.logits)
+        #torch.log(probs)
+        #print(probs)
+        loss = label * torch.log(probs+1e-10) + (1 - label) * torch.log(1 - probs+1e-10)
+        final_loss = -torch.mean(loss)
+
+        if return_outputs:
+            return final_loss, {"loss":final_loss}
+        return final_loss
+
+
+# Train the model, woohoo.
+trainer = RewardTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    #compute_metrics=compute_metrics,
+    data_collator=RewardDataCollatorWithPadding(
+        tokenizer=tokenizer, max_length=script_args.max_length),
+        lora_config=lora_config
+)
+
+
+trainer.train()
+
+
+print("Saving last checkpoint of the model")
+#model.save_pretrained(output_name + "/last_checkpoint")
+trainer.save_model(output_name + "/last_checkpoint")
+tokenizer.save_pretrained(output_name + "/last_checkpoint")
+
+
+
+
+
+# Train the model, woohoo.
+trainer = RewardTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    #compute_metrics=compute_metrics,
+    data_collator=RewardDataCollatorWithPadding(
+        tokenizer=tokenizer, max_length=script_args.max_length),
+)
+
+
+trainer.train()
+
+
+print("Saving last checkpoint of the model")
+#model.save_pretrained(output_name + "/last_checkpoint")
+trainer.save_model(output_name + "/last_checkpoint")
+tokenizer.save_pretrained(output_name + "/last_checkpoint")
